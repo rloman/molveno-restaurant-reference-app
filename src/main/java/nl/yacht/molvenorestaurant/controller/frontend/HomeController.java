679e2a252574894f6d88de2b3403f465077bfd9f
Raymond Loman <raymond@carpago.nl>
2018-06-29 13:37:21 +0200

    Add some demo for Mustache

--- src/main/java/nl/yacht/molvenorestaurant/controller/frontend/HomeController.java
+++ src/main/java/nl/yacht/molvenorestaurant/controller/frontend/HomeController.java
@@ -6,11 +6,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
+
+/*
+
+This controller is responsible for returning the correct template name based on the
+entered URL
+
+You ONLY use it for Mustache ...
+ */
 @Controller
 @RequestMapping(value="/")
 public class HomeController {
 
-    @RequestMapping(method= RequestMethod.GET)
+    // when I type in: http://localhost:8080/
+    // then it returns "index" which will then look
+    // in src/main/resources/templates and return the content
+    // of the file index.html
+    // while rendering the header and footer in using the
+    // mustache operator ( {{>header}} and {{>footer}}
+    @RequestMapping(value="/home", method= RequestMethod.GET)
     public String home(Map<String, Object> model){
         return "index";
     }
