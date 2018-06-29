209820d130da651bf2028f0aae9db25c8a622ea5
Raymond Loman <raymond@carpago.nl>
2018-06-29 11:56:36 +0200

    Nice for demo

--- src/main/java/nl/yacht/molvenorestaurant/controller/frontend/HomeController.java
+++ src/main/java/nl/yacht/molvenorestaurant/controller/frontend/HomeController.java
@@ -14,4 +14,24 @@ public class HomeController {
     public String home(Map<String, Object> model){
         return "index";
     }
+
+    @RequestMapping(value="/postget", method= RequestMethod.GET)
+    public String jqueryPostGet(Map<String, Object> model){
+        return "jquery-post-get";
+    }
+
+    @RequestMapping(value="/bootstrap", method= RequestMethod.GET)
+    public String bootstrap(Map<String, Object> model){
+        return "bootstrap-progress";
+    }
+
+    @RequestMapping(value="/jquery-demo", method= RequestMethod.GET)
+    public String jQueryDemo(Map<String, Object> model){
+        return "jquery-demo";
+    }
+
+    @RequestMapping(value="/bootstrap2", method= RequestMethod.GET)
+    public String bootstrap2(Map<String, Object> model){
+        return "bootstrap2";
+    }
 }
